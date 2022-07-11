138213477eaf23efaed1b13cc316eee98950436d
AndreFernandesDev <andre.fernandesdev@gmail.com>
2022-07-11 16:13:52 +0100

    release v.0.1

---
+++ controlpanel/src/routes/__layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '../app.css';
+	import Navbar from '$lib/components/Navbar.svelte';
+	import Footer from '$lib/components/Footer.svelte';
+</script>
+
+<Navbar />
+<main class="h-screen">
+	<slot />
+</main>
+<Footer />
---
+++ controlpanel/src/routes/index.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Card from '$lib/components/Card.svelte';
+	import Form from '$lib/components/Form.svelte';
+	import Modal from '$lib/components/Modal.svelte';
+</script>
+
+<Modal>
+	<Form />
+</Modal>
+<Card />
